71d30a622fa5b457b3aa782e87d3b6ab49b8c5d9
Volodymyr Chernikov <volodymyr3574@gmail.com>
2024-08-19 00:32:57 +0100

    Added Responsive App Bar Component

---
+++ party-match-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ResponsiveAppBar from "./ResponsiveAppBar";
+
+function App() {
+  return (
+    <>
+      <ResponsiveAppBar />
+    </>
+  );
+}
+
+export default App;
