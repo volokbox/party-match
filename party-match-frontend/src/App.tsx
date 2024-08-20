c7fc35e9f827a63290fd0f3286b63d53a9adbcdd
Volodymyr Chernikov <volodymyr3574@gmail.com>
2024-08-20 23:46:40 +0100

    App bar improvements and animation, React Router implementatoion

--- party-match-frontend/src/App.tsx
+++ party-match-frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import "./App.css";
-import ResponsiveAppBar from "./ResponsiveAppBar";
+import "./styles/App.css";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import ResponsiveAppBar from "./components/AppBar/ResponsiveAppBar";
 
 function App() {
   return (
-    <>
-      <ResponsiveAppBar />
-    </>
+    <HashRouter>
+      <div>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/coins" element={<></>} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
